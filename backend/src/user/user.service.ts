0db5bdef7d79add1e6a977e3c7e6464d88cdfb1e
João Vitor Souza Rocha <jvitorsouza18@gmail.com>
2022-08-01 19:14:13 -0300

    feat: :sparkles: deal with already registered users

--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import prisma from '../database/prisma';
 import { CreateUserDto } from './dto/create-user.dto';
 // import { UpdateUserDto } from './dto/update-user.dto';
@@ -17,18 +17,22 @@ export class UserService {
   }
 
   async create(createUserDto: CreateUserDto) {
-    return await prisma.user.create({ data: createUserDto });
+    try {
+      return await prisma.user.create({ data: createUserDto });
+    } catch (err) {
+      throw new HttpException('User already exists', HttpStatus.BAD_REQUEST);
+    }
+
+    // findOne(id: number) {
+    //   return `This action returns a #${id} user`;
+    // }
+
+    // update(id: number, updateUserDto: UpdateUserDto) {
+    //   return `This action updates a #${id} user`;
+    // }
+
+    // remove(id: number) {
+    //   return `This action removes a #${id} user`;
+    // }
   }
-
-  // findOne(id: number) {
-  //   return `This action returns a #${id} user`;
-  // }
-
-  // update(id: number, updateUserDto: UpdateUserDto) {
-  //   return `This action updates a #${id} user`;
-  // }
-
-  // remove(id: number) {
-  //   return `This action removes a #${id} user`;
-  // }
 }
