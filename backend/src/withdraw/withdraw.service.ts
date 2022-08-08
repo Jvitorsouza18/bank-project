ac21af227d3866c75ee6962bef9a133a953564e8
João Vitor Souza Rocha <jvitorsouza18@gmail.com>
2022-08-08 12:40:52 -0300

    feat: :sparkles: withdraw now uses prisma transactions

--- backend/src/withdraw/withdraw.service.ts
+++ backend/src/withdraw/withdraw.service.ts
@@ -1,3 +1,4 @@
+import { PrismaClient } from '@prisma/client';
 import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
 import prisma from 'src/database/prisma';
 import { CreateWithdrawDto } from './dto/create-withdraw.dto';
@@ -5,38 +6,40 @@ import { CreateWithdrawDto } from './dto/create-withdraw.dto';
 @Injectable()
 export class WithdrawService {
   async create(createWithdrawDto: CreateWithdrawDto, id: string) {
-    const insertTransaction = {
-      ...createWithdrawDto,
-      senderId: id,
-      type: 'withdraw',
-    };
+    await prisma.$transaction(async (prisma: PrismaClient) => {
+      const insertTransaction = {
+        ...createWithdrawDto,
+        senderId: id,
+        type: 'withdraw',
+      };
 
-    const value = Number(createWithdrawDto.value);
+      const value = Number(createWithdrawDto.value);
 
-    const targetUser = await prisma.user.findFirst({ where: { id } });
+      const targetUser = await prisma.user.findFirst({ where: { id } });
 
-    if (!targetUser) {
-      throw new HttpException('User does not exist', HttpStatus.BAD_REQUEST);
-    }
+      if (!targetUser) {
+        throw new HttpException('User does not exist', HttpStatus.BAD_REQUEST);
+      }
 
-    if (Number(targetUser.balance) < value) {
-      throw new HttpException(
-        "You don't have enough balance",
-        HttpStatus.BAD_REQUEST,
-      );
-    }
+      if (Number(targetUser.balance) < value) {
+        throw new HttpException(
+          "You don't have enough balance",
+          HttpStatus.BAD_REQUEST,
+        );
+      }
 
-    const newBalance = Number(targetUser.balance) - value;
+      const newBalance = Number(targetUser.balance) - value;
 
-    const insertBalance = {
-      balance: newBalance,
-    };
+      const insertBalance = {
+        balance: newBalance,
+      };
 
-    await prisma.user.update({
-      where: { id },
-      data: insertBalance,
+      await prisma.user.update({
+        where: { id },
+        data: insertBalance,
+      });
+
+      await prisma.transaction.create({ data: insertTransaction });
     });
-
-    await prisma.transaction.create({ data: insertTransaction });
   }
 }
